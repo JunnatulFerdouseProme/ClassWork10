18931fa1aad860d9988972151c0143c664fc8ae9
Afsarud <engafsar5577@gmail.com>
2019-09-29 20:06:05 +0600

    Add project files.

---
+++ MyWindowsFormsApp/BLL/ItemManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyWindowsFormsApp.Repository;
+
+namespace MyWindowsFormsApp.BLL
+{
+   public class ItemManager
+   {
+       private ItemRepository _itemRepository = new ItemRepository();
+
+       public bool Add(string name, double price)
+       {
+           return _itemRepository.Add(name, price);
+       }
+
+       public bool IsNameExist(string name)
+       {
+           return _itemRepository.IsNameExist(name);
+       }
+
+       public bool Update(string name, double price, int id)
+       {
+           return _itemRepository.Update(name, price, id);
+       }
+
+       public bool Delete(int id)
+       {
+           return _itemRepository.Delete(id);
+       }
+   }
+}
---
+++ MyWindowsFormsApp/ItemUi.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyWindowsFormsApp.BLL;
+using System.Windows.Forms;
+
+namespace MyWindowsFormsApp
+{
+    public partial class ItemUi : Form
+    {
+        ItemManager _itemManager=new ItemManager();
+        public ItemUi()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //Mandatory
+            if (String.IsNullOrEmpty(priceTextBox.Text))
+            {
+                MessageBox.Show("Price can not be Empty!!");
+                return;
+            }
+
+            //Unique
+            if (_itemManager.IsNameExist(nameTextBox.Text))
+            {
+                MessageBox.Show(nameTextBox.Text + " Already Exist!!");
+                return;
+            }
+
+            //Add/Insert
+            if (_itemManager.Add(nameTextBox.Text, Convert.ToDouble(priceTextBox.Text)))
+            {
+                MessageBox.Show("Saved");
+            }
+            else
+            {
+                MessageBox.Show("Not Saved");
+            }
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            Display();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            //Set Id as Mandatory
+            if (String.IsNullOrEmpty(idTextBox.Text))
+            {
+                MessageBox.Show("Id Can not be Empty!!!");
+                return;
+            }
+
+            //Delete
+            if (_itemManager.Delete(Convert.ToInt32(idTextBox.Text)))
+            {
+                MessageBox.Show("Deleted");
+            }
+            else
+            {
+                MessageBox.Show("Not Deleted");
+            }
+
+            Display();
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            //Set Id as Mandatory
+            if (String.IsNullOrEmpty(idTextBox.Text))
+            {
+                MessageBox.Show("Id Can not be Empty!!!");
+                return;
+            }
+            //Set Price as Mandatory
+            if (String.IsNullOrEmpty(priceTextBox.Text))
+            {
+                MessageBox.Show("Price Can not be Empty!!!");
+                return;
+            }
+
+            if (_itemManager.Update(nameTextBox.Text, Convert.ToDouble(priceTextBox.Text), Convert.ToInt32(idTextBox.Text)))
+            {
+                MessageBox.Show("Updated");
+                Display();
+            }
+            else
+            {
+                MessageBox.Show("Not Updated");
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            Search(nameTextBox.Text);
+        }
+
+        //Method
+      
+        
+        private void Display()
+        {
+            try
+            {
+                //Connection
+                string connectionString = @"Server=PC-301-04\SQLEXPRESS; Database=CoffeeShop; Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                //Command 
+                //INSERT INTO Items (Name, Price) Values ('Black', 120)
+                string commandString = @"SELECT * FROM Items";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                //Open
+                sqlConnection.Open();
+
+                //Show
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                if (dataTable.Rows.Count > 0)
+                {
+                    showDataGridView.DataSource = dataTable;
+                }
+                else
+                {
+                    MessageBox.Show("No Data Found");
+                }
+
+                //Close
+                sqlConnection.Close();
+
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            }
+        }
+       
+       
+        private void Search(string name)
+        {
+            try
+            {
+                //Connection
+                string connectionString = @"Server=PC-301-04\SQLEXPRESS; Database=CoffeeShop; Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                //Command 
+                //INSERT INTO Items (Name, Price) Values ('Black', 120)
+                string commandString = @"SELECT * FROM Items WHERE Name='" + name + "'";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                //Open
+                sqlConnection.Open();
+
+                //Show
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                if (dataTable.Rows.Count > 0)
+                {
+                    showDataGridView.DataSource = dataTable;
+                }
+                else
+                {
+                    MessageBox.Show("No Data Found");
+                }
+
+                //Close
+                sqlConnection.Close();
+
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            }
+        }
+
+     
+    }
+
+
+}
